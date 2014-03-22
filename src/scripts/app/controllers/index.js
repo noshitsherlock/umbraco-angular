ea16dd90d0006df5c760f431e0940ec70445c143
Tommy Albinsson <tommy.albinsson@gmail.com>
2014-03-22 14:43:39 +0100

    created smaller application

---
+++ src/scripts/app/controllers/index.js
@@ -0,0 +1,17 @@
+﻿app.controller("indexController", ['$scope', '$sce', 'UmbracoAngular', function($scope, $sce, UmbracoAngular) {
+    $scope.view = {};
+
+    $scope.GetNodeData = function (id) {
+        UmbracoAngular.GetNodeData(id).then(function (response) {
+            var data = response.data;
+
+            if (data.StatusMessage.Success) {
+                var property = UmbracoAngular.GetProperty("bodyText", data);
+                $scope.view.BodyText = $sce.trustAsHtml(property.Value);
+            } else {
+                $scope.view.BodyText = $sce.trustAsHtml("<p>" + data.StatusMessage.Message + "</p>");
+            }
+        });
+    };
+}
+]);
