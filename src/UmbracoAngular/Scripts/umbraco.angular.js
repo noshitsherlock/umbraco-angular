f58684e09d9e53a6e8fd031be63f20ecef19de33
Tommy Albinsson <tommy.albinsson@gmail.com>
2014-03-22 13:30:21 +0100

    Added solution and project

---
+++ src/UmbracoAngular/Scripts/umbraco.angular.js
@@ -0,0 +1,56 @@
+﻿/* global */
+
+'use strict';
+
+(function () {
+	var module = angular.module('umbraco.angular', []);
+
+	module.provider('UmbracoAngular', function () {
+		var Configurer = {};
+		Configurer.init = function (object, config) {
+			object.configuration = config;
+
+			config.apiEndpoint = config.apiEndpoint;
+			object.setApiEndpoint = function (apiEndPoint) {
+				config.apiEndpoint = apiEndPoint;
+			};
+		};
+
+		var globalConfiguration = {};
+
+		Configurer.init(this, globalConfiguration);
+
+		this.$get = ['$http', '$q', function ($http, $q) {
+			function createService(config) {
+				var service = {};
+
+				function GetNodeData(id) {
+					var deferred = $q.defer();
+
+					$http.get(config.apiEndpoint + id).then(function (response) {
+						deferred.resolve(response);
+					});
+
+					return deferred.promise;
+				}
+
+				function GetProperty(alias, data) {
+					for (var i = 0; i < data.Properties.length; i++) {
+						if (data.Properties[i].Alias == alias) {
+							return data.Properties[i];
+						}
+					}
+					return { Value: "Property not found" };
+				};
+
+				Configurer.init(service, config);
+				service.GetNodeData = GetNodeData;
+				service.GetProperty = GetProperty;
+				return service;
+			}
+
+			return createService(globalConfiguration);
+		}
+		];
+	});
+})();
